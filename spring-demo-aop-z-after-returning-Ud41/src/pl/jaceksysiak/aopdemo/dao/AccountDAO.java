775aaf57b2117bb5f373649e3f9d2731e7dd034f
Jacek <Jacek@BERLIN>
2020-03-05 20:21:11 +0100

    v

--- spring-demo-aop-z-after-returning-Ud41/src/pl/jaceksysiak/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-z-after-returning-Ud41/src/pl/jaceksysiak/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package pl.jaceksysiak.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import pl.jaceksysiak.aopdemo.Account;
@@ -9,6 +12,25 @@ public class AccountDAO {
 
 	private String name;
 	private String serviceCode;
+			
+	// add a new method: findAccounts()
+	
+	public List<Account> findAccounts() {
+		
+		List<Account> myAccounts = new ArrayList<>();
+		
+		// create sample accounts
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Platinum");
+		Account temp3 = new Account("Luca", "Gold");
+		
+		// add them to our accounts list
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+		
+		return myAccounts;		
+	}
 	
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		
@@ -42,4 +64,11 @@ public class AccountDAO {
 		this.serviceCode = serviceCode;
 	}
 	
+	
+	
 }
+
+
+
+
+
